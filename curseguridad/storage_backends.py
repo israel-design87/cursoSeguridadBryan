fcd263908a6e1ed910068c595e123c17c09984bd
Landevel <2520160008ilanderosc@gmail.com>
2025-06-23 22:23:49 -0600

    modificando  s3

--- curseguridad/storage_backends.py
+++ curseguridad/storage_backends.py
@@ -2,8 +2,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = "static"
-    default_acl = "public-read"
+    default_acl = None  # ✅ No usar ACLs
 
 class MediaStorage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
+    default_acl = None  # ✅ También sin ACLs
